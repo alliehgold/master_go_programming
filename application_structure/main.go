58c1d1832201c57428e1a8462eed82f8d32a1ff5
alliehgold <170181250+alliehgold@users.noreply.github.com>
2025-02-28 21:05:20 -0600

    added chapter 3 exercises

--- application_structure/main.go
+++ application_structure/main.go
@@ -20,3 +20,17 @@ func main() {
 }
 
 //you can use "go run main.go" to compile and run main.go
+//unlike "go build" it does not produce an executable
+//you may have to run "go mod init" first if you get an error
+//"go build" compiles the program but does not execute it
+//"go build -o appname" creates an executable with the specific name
+//Use "GOOS=<operating_system_name> go build" to build for a specific operating system
+//choices are Windows, linux, or darwin (for mac)
+//use "GOARCH=<architecturename>" to specify the CPU architecture such as AMD64 or ARM64
+//when specifying the name of the application, you do not need a file extension for linux or Mac
+
+//go strongly suggests certain styles
+//run "gofmt -w main.go" to have go format your application
+//use "gofmt -w -l <directory name>" to format every go file in a directory
+//just run "go fmt" to format all files in the current directory
+//vscode by default runs gofmt when you save a go file
---
+++ chapter_3_exercises/go.mod
@@ -0,0 +1,3 @@
+module chapter_3_exercises
+
+go 1.24.0
